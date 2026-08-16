2e5a90929a79ca864309c1b13dcf40a2e263e036
Musfirah kashan <101093830+musfirah-kashan@users.noreply.github.com>
2026-08-16 12:51:20 +0500

    Time: 19 ms (8.62%), Space: 28.1 MB (5.2%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+#include <unordered_set>
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int>s(nums.begin(),nums.end());
+        for (int i=0; i<=n; i++) {
+            if (s.find(i)==s.end()) 
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
