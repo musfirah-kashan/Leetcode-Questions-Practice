ebfc0a3428b65171b01c18a082e4151d8130ee84
Musfirah kashan <101093830+musfirah-kashan@users.noreply.github.com>
2026-09-16 19:45:03 +0500

    Time: 0 ms (100%), Space: 8.9 MB (37.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>s1;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                s1.push(s[i]);
+            }
+            else{
+                if(s1.size()==0){
+                    return false;
+                }
+
+                if((s1.top()=='('&& s[i]==')')||
+                (s1.top()=='{'&& s[i]=='}')||
+                (s1.top()=='['&& s[i]==']')){
+                    s1.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return s1.size()==0;
+    }
+};
