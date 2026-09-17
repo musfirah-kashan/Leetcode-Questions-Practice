5dfff04dfb742445f770bee1834addaac035d48e
Musfirah kashan <101093830+musfirah-kashan@users.noreply.github.com>
2026-09-17 19:34:16 +0500

    Time: 0 ms (100%), Space: 19.2 MB (46.7%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        res=[]
+        for i in s:
+            if i not in res:
+                res.append(i)
+            else:
+                return i    
