c7b4062417f3faa650163aa3f06899355cb415b3
Musfirah kashan <101093830+musfirah-kashan@users.noreply.github.com>
2026-09-24 20:44:52 +0500

    Time: 0 ms (100%), Space: 8.6 MB (71.78%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,6 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // double res=1;
-        // long long exp=n;
-        // if(n>=0){
-        //    for(long long i=1; i<=exp; i++){
-        //     res*=x;
-        //    } 
-        // }else{
-        //     for(long long i=1; i<=-exp; i++){
-        //         res*=x;
-        //     }
-        //     res=1/res;
-        // }
-        // return res;  //slow working
-        
-
       double res = 1.0;
         long long exp = n;
         if (exp < 0) {
